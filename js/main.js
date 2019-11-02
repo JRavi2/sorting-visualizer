75a526bd6310c60799818dc2a57523ee59bc3f5a
Ravi Jain <jravi248@gmail.com>
2019-11-02 23:15:52 +0530

    Added Insertion Sort

--- js/main.js
+++ js/main.js
@@ -8,12 +8,16 @@ const sortBtn = document.querySelector("#sort");
 // Algorithm Selectors
 const selSortBtn = document.querySelector("#sel-sort");
 const bubbleSortBtn = document.querySelector("#bubble-sort");
-const mergeSortBtn = document.querySelector("#merge-sort");
+// const mergeSortBtn = document.querySelector("#merge-sort");
 const insertSortBtn = document.querySelector("#insert-sort");
 const quickSortBtn = document.querySelector("#quick-sort");
 
 // Controller Variables
-const ARRAY_SIZE = 80;
+const BLUE = "rgb(74, 74, 252)";
+const PURPLE = "rgb(199, 38, 92)";
+const GREEN = "green";
+const RED = "rgb(243, 59, 59)";
+const ARRAY_SIZE = 20;
 var algo = "select";
 
 // Initialize the array
@@ -25,7 +29,7 @@ for (var a = [], i = 0; i < ARRAY_SIZE; ++i) {
 }
 
 // Utitlity Timer Function
-const Timer = ms => {
+const Sleep = ms => {
     return new Promise(res => setTimeout(res, ms));
 };
 
@@ -33,7 +37,7 @@ const Timer = ms => {
 const DeselectAll = () => {
     selSortBtn.classList.add("options-us");
     bubbleSortBtn.classList.add("options-us");
-    mergeSortBtn.classList.add("options-us");
+    // mergeSortBtn.classList.add("options-us");
     insertSortBtn.classList.add("options-us");
     quickSortBtn.classList.add("options-us");
 };
@@ -54,10 +58,10 @@ const Shuffle = array => {
 };
 
 // Render the bars
-const Render = () => {
+const Render = color => {
     const bars = document.querySelectorAll(".bar");
-    for (i = 0; i < ARRAY_SIZE; i++) {
-        bars[i].style = "height: " + a[i] + "vh;";
+    for (x = 0; x < ARRAY_SIZE; x++) {
+        bars[x].style = `background: ${color};height: ${a[x]}vh;`;
     }
 };
 
@@ -69,17 +73,19 @@ async function SelectionSort() {
         for (k = j + 1; k < ARRAY_SIZE; k++)
             if (a[k] < a[min_index]) min_index = k;
 
-        bars[j].style = "background: rgb(243, 59, 59); height: " + a[j] + "vh;";
-        bars[min_index].style =
-            "background: rgb(243, 59, 59); height: " + a[min_index] + "vh;";
+        bars[j].style = `background: ${RED}; height: ${a[j]}vh;`;
+        bars[
+            min_index
+        ].style = `background: ${RED}; height: ${a[min_index]}vh;`;
 
         var temp = a[j];
         a[j] = a[min_index];
         a[min_index] = temp;
-        await Timer(100);
-        bars[min_index].style =
-            "background: rgb(74, 74, 252); height: " + a[min_index] + "vh;";
-        bars[j].style = "background: rgb(199, 38, 92); height: " + a[j] + "vh;";
+        await Sleep(100);
+        bars[
+            min_index
+        ].style = `background: ${BLUE}; height: ${a[min_index]}vh;`;
+        bars[j].style = `background: ${PURPLE}; height: ${a[j]}vh;`;
     }
 }
 
@@ -93,35 +99,149 @@ async function BubbleSort() {
                 const temp = a[j];
                 a[j] = a[j + 1];
                 a[j + 1] = temp;
-                bars[j].style = "background: red; height: " + a[j] + "vh;";
-                bars[j + 1].style =
-                    "background: red; height: " + a[j + 1] + "vh;";
-                await Timer(25);
-                bars[j].style =
-                    "background: rgb(74, 74, 252); height: " + a[j] + "vh;";
-                bars[j + 1].style =
-                    "background: rgb(74, 74, 252); height: " + a[j + 1] + "vh;";
+                bars[j].style = `background: ${RED}; height: ${a[j]}vh;`;
+                bars[j + 1].style = `background: ${RED}; height: ${
+                    a[j + 1]
+                }vh;`;
+                await Sleep(25);
+                bars[j].style = `background: ${BLUE}; height: ${a[j]}vh;`;
+                bars[j + 1].style = `background: ${BLUE}; height: ${
+                    a[j + 1]
+                }vh;`;
             } else {
-                bars[j].style = "background: green; height: " + a[j] + "vh;";
-                bars[j + 1].style =
-                    "background: green; height: " + a[j + 1] + "vh;";
-                await Timer(25);
-                bars[j].style =
-                    "background: rgb(74, 74, 252); height: " + a[j] + "vh;";
-                bars[j + 1].style =
-                    "background: rgb(74, 74, 252); height: " + a[j + 1] + "vh;";
+                bars[j].style = `background: ${GREEN}; height: ${a[j]}vh;`;
+                bars[j + 1].style = `background: ${GREEN}; height: ${
+                    a[j + 1]
+                }vh;`;
+                await Sleep(25);
+                bars[j].style = `background: ${BLUE}; height: ${a[j]}vh;`;
+                bars[j + 1].style = `background: ${BLUE}; height: ${
+                    a[j + 1]
+                }vh;`;
             }
         }
-        bars[ARRAY_SIZE - i - 1].style =
-            "background: rgb(199, 38, 92); height: " +
-            a[ARRAY_SIZE - i - 1] +
-            "vh;";
+        bars[ARRAY_SIZE - i - 1].style = `background: ${PURPLE}; height: ${
+            a[ARRAY_SIZE - i - 1]
+        }vh;`;
     }
-    bars[0].style = "background: rgb(199, 38, 92); height: " + a[0] + "vh;";
+    bars[0].style = `background: ${PURPLE}; height: ${a[0]}vh;`;
 }
 
-// Merge Sort
-async function MergeSort() {}
+// Merge Sort (Not Working, To be Fixed)
+// function Merge(l, m, r) {
+//     var i, j, k;
+//     var n1 = m - l + 1;
+//     var n2 = r - m;
+
+//     /* create temp arrays */
+//     var L = [],
+//         R = [];
+
+//     /* Copy data to temp arrays L[] and R[] */
+//     for (i = 0; i < n1; i++) L[i] = a[l + i];
+//     for (j = 0; j < n2; j++) R[j] = a[m + 1 + j];
+
+//     /* Merge the temp arrays back into arr[l..r]*/
+//     i = 0; // Initial index of first subarray
+//     j = 0; // Initial index of second subarray
+//     k = l; // Initial index of merged subarray
+//     while (i < n1 && j < n2) {
+//         if (L[i] <= R[j]) {
+//             a[k] = L[i];
+//             i++;
+//         } else {
+//             a[k] = R[j];
+//             j++;
+//         }
+//         k++;
+//     }
+
+//     /* Copy the remaining elements of L[], if there
+//        are any */
+//     while (i < n1) {
+//         a[k] = L[i];
+//         i++;
+//         k++;
+//     }
+
+//     /* Copy the remaining elements of R[], if there
+//        are any */
+//     while (j < n2) {
+//         a[k] = R[j];
+//         j++;
+//         k++;
+//     }
+// }
+
+// /* l is for left index and r is right index of the
+//    sub-array of arr to be sorted */
+// function MergeSort(l, r) {
+//     if (l < r) {
+//         // Same as (l+r)/2, but avoids overflow for
+//         // large l and h
+//         var m = l + (r - l) / 2;
+
+//         // Sort first and second halves
+//         MergeSort(l, m);
+//         MergeSort(m + 1, r);
+
+//         Merge(l, m, r);
+//         console.log(a);
+//     }
+// }
+
+// Insertion Sort
+async function InsertionSort() {
+    const bars = document.querySelectorAll(".bar");
+    var key;
+    for (i = 0; i < ARRAY_SIZE; i++) {
+        key = a[i];
+        j = i - 1;
+        bars[i].style = `background: ${RED}; height: ${a[i]}vh;`;
+        await Sleep(500);
+        while (j >= 0 && a[j] > key) {
+            bars[j].style = `background: ${RED}; height: ${a[j]}vh;`;
+            Sleep(100);
+            bars[j].style = `background: ${BLUE}; height: ${a[j]}vh;`;
+            a[j + 1] = a[j];
+            j--;
+        }
+        bars[j + 1].style = `background: ${GREEN}; height: ${a[j + 1]}vh;`;
+        await Sleep(500);
+        a[j + 1] = key;
+        Render(BLUE);
+    }
+    Render(PURPLE);
+}
+
+// Quick Sort
+const partition = async (low, high) => {
+    const pivot = a[high];
+    i = low - 1;
+    console.log(i);
+    for (j = 0; j < high; j++) {
+        if (a[j] < pivot) {
+            i++;
+            const temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+    const temp = a[j];
+    a[j] = a[i + 1];
+    a[i + 1] = temp;
+    console.log(i + 1);
+    return i + 1;
+};
+
+function QuickSort(low, high) {
+    if (low < high) {
+        const pi = partition(low, high);
+        console.log(a);
+        QuickSort(low, pi - 1);
+        QuickSort(pi + 1, high);
+    }
+}
 
 // Run the appropriate Function according to the Selected Algorithm
 const Sort = () => {
@@ -132,20 +252,22 @@ const Sort = () => {
         case "bubble":
             BubbleSort();
             break;
-        case "merge":
-            MergeSort();
-            break;
+        // case "merge":
+        //     MergeSort(0, ARRAY_SIZE - 1);
+        //     Render();
+        //     break;
         case "insert":
             InsertionSort();
             break;
         case "quick":
-            QuickSort();
+            QuickSort(0, ARRAY_SIZE - 1);
+            Render();
             break;
     }
 };
 
 a = Shuffle(a);
-Render();
+Render(BLUE);
 
 randomizeBtn.addEventListener("click", e => {
     Shuffle(a);
@@ -164,12 +286,12 @@ bubbleSortBtn.addEventListener("click", () => {
     bubbleSortBtn.classList.remove("options-us");
     bubbleSortBtn.classList.add("options-se");
 });
-mergeSortBtn.addEventListener("click", () => {
-    algo = "merge";
-    DeselectAll();
-    mergeSortBtn.classList.remove("options-us");
-    mergeSortBtn.classList.add("options-se");
-});
+// mergeSortBtn.addEventListener("click", () => {
+//     algo = "merge";
+//     DeselectAll();
+//     mergeSortBtn.classList.remove("options-us");
+//     mergeSortBtn.classList.add("options-se");
+// });
 insertSortBtn.addEventListener("click", () => {
     algo = "insert";
     DeselectAll();
